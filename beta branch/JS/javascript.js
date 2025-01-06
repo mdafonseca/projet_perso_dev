7307be17e89a4f8eeec2ec67b5e6f0f10a6417d1
Zelitrem <zelitrem@gmail.com>
2025-01-06 19:20:56 +0100

    Update Caroussel

--- beta branch/JS/javascript.js
+++ beta branch/JS/javascript.js
@@ -1,25 +1,25 @@
 // CAROUSEL IMAGE
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const items = document.querySelectorAll(".carousel-item");
-//     let currentIndex = 1; // L'image centrale par défaut
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".carousel-item");
+    let currentIndex = 1; // L'image centrale par défaut
   
-//     const updateCarousel = () => {
-//       items.forEach((item, index) => {
-//         const newIndex = (index - currentIndex + items.length) % items.length;
-//         item.dataset.index = newIndex;
-//       });
-//     };
+    const updateCarousel = () => {
+      items.forEach((item, index) => {
+        const newIndex = (index - currentIndex + items.length) % items.length;
+        item.dataset.index = newIndex;
+      });
+    };
   
-//     items.forEach((item) => {
-//       item.addEventListener("click", () => {
-//         const clickedIndex = parseInt(item.dataset.index, 10);
-//         if (clickedIndex !== 1) {
-//           currentIndex = (currentIndex + clickedIndex - 1 + items.length) % items.length;
-//           updateCarousel();
-//         }
-//       });
-//     });
+    items.forEach((item) => {
+      item.addEventListener("click", () => {
+        const clickedIndex = parseInt(item.dataset.index, 10);
+        if (clickedIndex !== 1) {
+          currentIndex = (currentIndex + clickedIndex - 1 + items.length) % items.length;
+          updateCarousel();
+        }
+      });
+    });
   
-//     updateCarousel();
-//   });
+    updateCarousel();
+  });
